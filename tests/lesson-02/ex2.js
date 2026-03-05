69c44e1fe9e26ae0ce55ad2ac121d4e5834536a1
Ngoc Vo <kimngocvo247@gmail.com>
2026-03-05 18:02:01 +0700

    init project

---
+++ tests/lesson-02/ex2.js
@@ -0,0 +1,9 @@
+/*const myName = "Alex";
+myName = "Nagi";
+
+console.log(myName);*/ //Sai o const, vi const di voi mot gia tri co dinh. Let bang khai bao, con khi ghi thang myName thi la gan lai, nen o terminal se hien thi Nagi
+
+let myName = "Alex";
+myName = "Nagi";
+
+console.log(myName);
---
+++ tests/lesson-02/ex3.js
@@ -0,0 +1,8 @@
+let a = true ;
+let b = false ;
+let c = true ;
+
+console.log (a && b && c);
+console.log (a && b || c);
+
+//Result se la false va true
